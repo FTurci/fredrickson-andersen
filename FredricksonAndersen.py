06722eb81c4d929bf2d0becb07141a9e5593ee94
Francesco Turci <f.turci@bristol.ac.uk>
2019-05-19 11:08:33 +0100

    latest

--- FredricksonAndersen.py
+++ FredricksonAndersen.py
@@ -2,28 +2,37 @@ from __future__ import print_function
 import numpy as np
 #from numba import jit
 import sys
-
+import copy
 np.random.seed(0)
 
 class FA1d(object):
+  """Fredrickson-Andersen 1d kinetically constrained model."""
   def __init__(self,N,T):
+    """Initialise the lattice. Parameters:
+    N : number of sites
+    T : temperature
+    """
     super(FA1d, self).__init__()
     self.N = N
     self.T = T
     self.beta = 1./T
     self.lattice = np.zeros(N)
     self.lattice[0]=1
-    self.facilitation()
     self.boltzmann = np.exp(-self.beta)
     self.time_integrated_excitations = 0.0
+    self.facilitation()
     
   def set(self,conf):
-    self.lattice = conf
+    """Assign a configuration to the lattice."""
+    self.lattice = np.copy(conf)
+    self.facilitation()
+
   def get(self):
+    """Obtain a lattice configuration."""
     return self.lattice
-    
+
   def load(self,filename, snapshot= -1):
-    """Loadsystem's state from file"""
+    """Load system's state from file"""
     print ("Loading",filename)
     table = np.loadtxt(filename)
     assert table.shape[1] == self.N
@@ -31,62 +40,76 @@ class FA1d(object):
     self.facilitation()
 
   def at(self,value):
+    """Consider periodic boundary conditions when computing the lattice site."""
     return value%self.N
   
   def fw_probabilitiy(self,site):
+    """Compute the jump probability forward in time."""
+    if self.lattice[site]==0:
+      """If the spin is 0 and the constraint is satisifed it flips with a Boltzmann probability"""
+      return self.constraint[site]*self.boltzmann
+    else:
+      """If the spin is 1 it flips if the constraint is satisfied"""
+      return self.constraint[site]
+  
+  def bw_probabilitiy(self,site):
     if self.lattice[site]==0:
       return self.constraint[site]*self.boltzmann
     else:
       return self.constraint[site]
   
-  def bw_probabilitiy(self,site):
-    if self.lattice[site]==1:
-      return self.constraint[site]*self.boltzmann
-    else:
-      return self.constraint[site]
-  
-
+  @jit(nopython=True)
   def flip(self, site):
-    self.lattice[site] = 0 if self.lattice[site] else 1
-    
-    if self.lattice[site]==1:
-      self.constraint[self.at(site-1)] = 1
-      self.constraint[self.at(site+1)]= 1
+    #self.lattice[site] =0 if self.lattice[site]==1 else 1
+    #self.facilitation()
+
+    if self.lattice[site]==0:
+      self.lattice[site]=1 #flip
+      self.constraint[self.at(site+1)]=1
+      self.constraint[self.at(site-1)]=1
     else:
+      self.lattice[site]=0 #flip
       for i in [-1,0,1]:
         idx = self.at(site+i)
         idm1 = self.lattice[self.at(idx-1)]
         idp1 = self.lattice[self.at(idx+1)]
         self.constraint[idx] =idp1+idm1-idm1*idp1
 
-  def facilitation(self):
+  def facilitation(self): 
     np1=np.roll(self.lattice,-1)
     nm1=np.roll(self.lattice,1)
     self.constraint = np1+nm1-np1*nm1
   
 
-  #@jit(nopython=False)
+  @jit(nopython=True)
   def fw_run(self,nsweeps, integrate_excitations=False):
+    # print("Forward")
     for i in range(nsweeps):
       for p in range(self.N):
         #pick a site
         s = np.random.randint(self.N)
         pb = self.fw_probabilitiy(s)
+        # print(s,pb,self.lattice,self.constraint)
         #print(s,pb, self.lattice[s])
         if np.random.uniform() < pb:
           self.flip(s)
         if integrate_excitations:
           self.time_integrated_excitations += self.lattice.sum()
   
+  @jit(nopython=True) 
   def bw_run(self,nsweeps):
     for i in range(nsweeps):
       for p in range(self.N):
         #pick a site
         s = np.random.randint(self.N)
         pb = self.bw_probabilitiy(s)
-        print(s,pb, self.lattice[s])
+        # print(s,pb, self.lattice[s])
         if np.random.uniform() < pb:
           self.flip(s)
+
+  @jit(nopython=True)
+  def continous_fw():
+
 
   def status(self,constraints=False):
     print ("sites ")
@@ -104,6 +127,7 @@ class FA1d(object):
     nflips =int(value*self.N)
     self.lattice[:nflips]=1
     np.random.shuffle(self.lattice)
+    self.facilitation()
 
   def density(self):
     return np.sum(self.lattice)/self.N
@@ -112,8 +136,11 @@ class FA1d(object):
   def compare_density(self):
     print("Observation %.2f Theory %.2f"%(self.density(), 1./(1+1./self.boltzmann)))
 
-#model = FA1d(180,1.0)
-#model.set_density(model.theoretical_density())
+model = FA1d(10,1.0)
+
+model.set_density(model.theoretical_density())
+print (model.lattice)
+print (model.constraint)
 #fout = open("kymo.txt", 'w')
 #for i in range(int(sys.argv[1])):
   #model.status()
--- kcmtrajectory.py
+++ kcmtrajectory.py
@@ -1,8 +1,10 @@
+from __future__ import print_function
 from FredricksonAndersen import FA1d
 import pickle
 import tqdm
 import numpy as np
 import copy
+import pylab as pl
 #from joblib import Parallel, delayed
 
 
@@ -13,75 +15,110 @@ class KCMtrajectory(object):
     self.model = model
     self.tobs = tobs
     self.kymograph = np.zeros((tobs,len(model.get())))
+    self.constraints = np.zeros((tobs,len(model.get())))
 
   def spawn(self):
     for i in range(tobs):
       self.kymograph[i]=self.model.get()
+      self.constraints[i] = self.model.constraint
       model.fw_run(1)
-    self.excitations = self.kymograph.sum()
+    
 
   def excitation_density(self):
-    return self.excitations/np.prod(self.kymograph.shape) 
-    
+    return self.excitations()/np.prod(self.kymograph.shape) 
+  def excitations(self): 
+    return self.kymograph.sum()
 
 class KCMtps(object):
   """docstring for KCMtps"""
-  def __init__(self, trajectory,mcsteps):
+  def __init__(self, trajectory, field,log=False):
     super(KCMtps, self).__init__()
     self.trajectory = trajectory
-    self.mcsteps = mcsteps
+    self.field=field    
+    #self.mcsteps = mcsteps
     self.length = trajectory.kymograph.shape[1]
     self.tobs = trajectory.kymograph.shape[0]
+    self.log = log
 
-
-  def fw_shoot(self):
-    r = np.random.randint(self.tobs)
+  def fw_shoot(self,margin=2):
+    r = np.random.randint(margin,self.tobs-margin)
+    if self.log:
+      print ("forward",r) 
     #new trajectory
-    trajcopy = copy.copy(self.trajectory)
+    trajcopy = copy.deepcopy(self.trajectory)
     trajcopy.model.set(trajcopy.kymograph[r])
-    for i in range(r+1,self.tobs):
-      trajcopy[i] = trajcopy.model.fw_run(1) 
-    self.trajectory = trajcopy
 
-  def bw_shoot(self):
-    r = np.random.randint(self.tobs)
+    for i in range(r+1,self.tobs):
+      trajcopy.model.fw_run(1)
+      # print (i)
+      trajcopy.kymograph[i] =  trajcopy.model.get()
+
+    #self.trajectory = trajcopy
+    return trajcopy
+
+  def bw_shoot(self,margin=2):
+    r = np.random.randint(margin,self.tobs-margin)
+    if self.log:
+      print ("backward",r)
     #new trajectory
-    trajcopy = copy.copy(self.trajectory)
+    trajcopy = copy.deepcopy(self.trajectory)
     trajcopy.model.set(trajcopy.kymograph[r])
     for i in range(r-1,0, -1):
-      trajcopy[i] = trajcopy.model.bw_run(1) 
-    self.trajectory = trajcopy
+      trajcopy.model.bw_run(1) 
+      trajcopy.kymograph[i] = trajcopy.model.get()
+    
+    return trajcopy
+    #self.trajectory = trajcopy
 
   def fw_shift(self):
     pass
   def bw_shift(self):
     pass
 
-  
+  def metropolis(self):
+    if np.random.uniform()<0.5:
+      attempt = self.fw_shoot()
+    else:
+      attempt = self.bw_shoot()
+    r = np.random.uniform()
+    
+   
+    if r <np.exp(-self.field*attempt.excitations()):
+      self.trajectory = attempt
+      
+      
 
-N = 10
+N = 30
 L = N
 tobs=320
 ntrajectories = 1000
+mcsteps = 100000
 model = FA1d(N,1.0)
 model.set_density(model.theoretical_density())
 # #fout = open("kymo.txt", 'w')
 # # equilibrate
-model.fw_run(10*tobs)
+# model.fw_run(10*tobs)
+# create the first trajectory
 X =KCMtrajectory(model, tobs)
 X.spawn()
-# model.load("equilibrated.txt")
-# values= []
-# for i in tqdm.tqdm(range(ntrajectories)):
-#   model.fw_run(tobs, integrate_excitations=True)
-#   values.append(model.time_integrated_excitations/float(N*L*tobs))
 
-
-# pickle.dump(values,open("values.p",'w'))
-
-# import pylab as pl
-
-# pl.hist(values,bins=30)
+# pl.imshow(X.kymograph)
+# pl.figure()
+# pl.imshow(X.constraints, cmap=pl.cm.Paired)
 # pl.show()
-  #model.fw_run(1)
-  #model.write(fout)
+TPS = KCMtps(X, 0.0001,log= False)
+
+values=[]
+#for i in tqdm.tqdm(range(mcsteps)):
+for i in range(mcsteps):
+  TPS.metropolis()
+  values.append(TPS.trajectory.excitation_density())
+  print (i, np.mean(values))
+#pl.imshow(TPS.trajectory.kymograph)
+
+#TPS.bw_shoot()
+#pl.figure()
+#pl.imshow(TPS.trajectory.kymograph)#cmap=pl.cm.Paired)
+#pl.show()
+
+
